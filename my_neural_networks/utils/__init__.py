6e11c60cf4fd2a8c61c8066c6ceee13e48bfbf2e
Ted Pinkerton <ted.pinkerton@mail.utoronto.ca>
2023-04-06 11:38:34 -0400

    refactored

--- my_neural_networks/utils/__init__.py
+++ my_neural_networks/utils/__init__.py
@@ -1,10 +1,11 @@
 """Utilities for the neural networks package."""
 
 # Import all the utils
-from errors import CompileError, CompiledError
+from exceptions import NetworkError, CompileError, CompiledError
 
 
 # Define the __all__ variable
-__all__ = [CompileError,
+__all__ = [NetworkError,
+           CompileError,
            CompiledError
            ]
--- my_neural_networks/utils/exceptions.py
+++ my_neural_networks/utils/exceptions.py
@@ -1,8 +1,13 @@
-class CompileError(Exception):
+class NetworkError(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class CompileError(NetworkError):
     """Exception raised when the neural network cannot be compiled."""
     pass
 
 
-class CompiledError(Exception):
+class CompiledError(NetworkError):
     """Exception raised when the neural network has not been compiled."""
     pass
